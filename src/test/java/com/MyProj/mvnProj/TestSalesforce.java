a9f62e8fcc4026e035ec6f8bde24f8835376c442
suruchi <suruchiagrawal07@gmail.com>
2017-04-22 15:46:10 +0530

    Commiting Mvn proj

---
+++ src/test/java/com/MyProj/mvnProj/TestSalesforce.java
@@ -0,0 +1,28 @@
+package com.MyProj.mvnProj;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.*;
+
+public class TestSalesforce {
+	@Test
+	public void Test()
+	{
+		String Path=System.getProperty("user.dir");
+	System.setProperty("webdriver.chrome.driver",Path+"\\chromedriver_win32\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	
+	driver.get("http://test.salesforce.com");
+	
+	driver.findElement(By.id("username")).sendKeys("suruchi.agrawal@accenture.com.smip.qa");
+	driver.findElement(By.id("password")).sendKeys("Testing123456");
+	
+	driver.findElement(By.id("Login")).click();
+	
+	
+	
+	
+}
+}
